d49197ea7284d79d0dd54a44674a25765ca78892
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-04-21 14:04:21 +0300

    Working with events and two-way binding

--- MovieApp/src/app/movies/movies.component.ts
+++ MovieApp/src/app/movies/movies.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { Movies } from '../movie.datasource';
+import { Movie } from '../movie';
 
 @Component({
     selector: 'movies', // <movies></movies>
     templateUrl: 'movies.component.html'
 })
-export class MoviesComponent{
+export class MoviesComponent {
     title = "Movie List";
     movies = Movies;
+    selectedMovie: Movie;
+
+    onSelect(movie: Movie): void {
+        this.selectedMovie = movie;
+    }
 }
