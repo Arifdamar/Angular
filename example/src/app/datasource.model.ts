6d668560db1384a7c1d53afffd1bf7f26a56778c
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-04-28 00:10:51 +0300

    Class Binding

--- example/src/app/datasource.model.ts
+++ example/src/app/datasource.model.ts
@@ -8,7 +8,7 @@ export class SimpleDataSource {
             new Product(1, "Samsung Galaxy S8", "Good phone", "s8.jpg", 5000),
             new Product(2, "Samsung Galaxy S8+", "Good phone", "s8+.jpg", 5500),
             new Product(3, "Samsung Galaxy S9", "Good phone", "s9.jpg", 6000),
-            new Product(4, "Samsung Galaxy S9+", "Good phone", "s9+.jpg", 6500),
+            new Product(4, "Samsung Galaxy S9+", "Good phone", "s9+.jpg", 16500),
             new Product(5, "Samsung Galaxy S10", "The best phone", "s10.jpg", 7000),
         );
     }
--- example/src/app/product.component.ts
+++ example/src/app/product.component.ts
@@ -9,6 +9,20 @@ import { Product } from './product.model';
 })
 export class ProductComponent {
     model: ProductRepository = new ProductRepository();
+    disabled = true;
 
-    product: Product = this.model.getProductById(1);
+    getClasses(id: number): string {
+        let product: Product = this.model.getProductById(id);
+        return (product.price <= 6000 ? "bg-info" : "bg-secondary") + " m-2 p-2";
+    }
+
+    getClassMap(id: number): Object {
+        let product: Product = this.model.getProductById(id);
+        return { 
+            "bg-info": product.price <= 6000,
+            "bg-secondary": product.price > 6000,
+            "text-center text-white": product.name == "Samsung Galaxy S9+"
+        }
+    }
+
 }
