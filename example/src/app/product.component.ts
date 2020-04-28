6e1cf1108926e6dbe23e8404c1836d756a08a17f
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-04-28 13:15:33 +0300

    Two-way binding

--- example/src/app/product.component.ts
+++ example/src/app/product.component.ts
@@ -4,50 +4,20 @@ import { Product } from './product.model';
 
 @Component({
     selector: 'app',
-    templateUrl: './product.component.html',
+    template: `
+            <input [(ngModel)]="email" (keyup.enter)="onKeyup()">
+            <br>
+            <span> {{ email }} </span>
+            `,
     styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
     model: ProductRepository = new ProductRepository();
     disabled = true;
 
-    onKeyup(value) {
-        console.log(value);
-    }
-
-    onSubmit($event) {
-        $event.stopPropagation();
-        console.log("Button clicked!");
-        console.log($event);
-    }
-
-    onDivClicked() {
-        console.log("Div clicked!");
-    }
-
-    getClasses(id: number): string {
-        let product: Product = this.model.getProductById(id);
-        return (product.price <= 6000 ? "bg-info" : "bg-secondary") + " m-2 p-2";
-    }
-
-    getClassMap(id: number): Object {
-        let product: Product = this.model.getProductById(id);
-        return {
-            "bg-info": product.price <= 6000,
-            "bg-secondary": product.price > 6000,
-            "text-center text-white": product.name == "Samsung Galaxy S9+"
-        }
-    }
-
-    color: string = this.model.getProductById(1).price <= 6000 ? "green" : "red";
-    fontSize: string = "25px";
-
-    getStyles(id: number) {
-        let product: Product = this.model.getProductById(id);
-        return {
-            fontSize: "25px",
-            color: product.price <= 6000 ? "green" : "red"
-        }
+    email: string = "info@arifdamar.com"
+    onKeyup() {
+        console.log(this.email);
     }
 
 }
