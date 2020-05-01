411e81641dd919936c2a90ad0a94de5f97d4ffe5
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-05-01 15:18:39 +0300

    ngIf directive

--- example/src/app/product.component.ts
+++ example/src/app/product.component.ts
@@ -1,20 +1,13 @@
 import { Component } from '@angular/core';
 import { ProductRepository } from './repository.model';
-import { Product } from './product.model';
 
 @Component({
     selector: 'app',
-    template: `
-    {{ text | summary }}
-    <br>
-    {{ text | summary: 50 }}
-    <br>
-    {{ text | summary: 100 }}
-            `,
+    templateUrl: './product.component.html',
     styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
 
-    text = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero sequi et placeat, accusamus rem, necessitatibus cum delectus odio ex aspernatur saepe laboriosam modi, iure doloremque facere. Est fuga expedita illo?";
-
+    model: ProductRepository = new ProductRepository();
+    
 }
