45e01de745a19c70640e22ec44be66475c1ef435
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-05-02 14:43:45 +0300

    Listing products

--- example/src/app/app.module.ts
+++ example/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ProductComponent } from './product.component';
 import { FormsModule } from '@angular/forms';
-import { SummaryPipe } from './summary.pipe';
-import { InputEmailDirective } from './input-email.directive';
+import { AdminProductsComponent } from './admin-products/admin-products.component';
 
 
 @NgModule({
   declarations: [
     ProductComponent,
-    SummaryPipe,
-    InputEmailDirective
+    AdminProductsComponent
   ],
   imports: [
     BrowserModule,
