d3d8969619611e09031921aa80316c9ef77a1ae2
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-05-12 02:14:46 +0300

    Post data

--- example2/src/app/post/post.component.ts
+++ example2/src/app/post/post.component.ts
@@ -1,23 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
 @Component({
   selector: 'post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css', "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"]
 })
-export class PostComponent{
+export class PostComponent {
 
-  posts;
+  posts: [any];
+  private url = 'https://jsonplaceholder.typicode.com/posts';
+
   constructor(private http: HttpClient) {
 
-    
-    http.get('https://api.open5e.com/spells/')
-    .subscribe(response => {
-      console.log(response);
-      this.posts = response;
-    })
+    http.get(this.url)
+      .subscribe(response => {
+        this.posts = <[any]>response;
+      })
+
   }
 
+  createPost(input: HTMLInputElement) {
+    const post = { title: input.value }
+    input.value = '';
+
+    this.http.post(this.url + '/', JSON.stringify(post))
+      .subscribe(response => {
+        post['id'] = response['id'];
+        this.posts.splice(0, 0, post);
+      })
+
+  }
 
 }
