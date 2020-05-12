af1a848131730afbf8da324d6de8194660ef31f0
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-05-12 16:13:35 +0300

    Adding admin module

--- example2/src/app/app.module.ts
+++ example2/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ProductComponent } from './products/product/product.component';
 import { UserComponent } from './users/user/user.component';
 import { EditProductComponent } from './products/edit-product/edit-product.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
+    AdminModule,
     AppRoutingModule
   ],
   providers: [],
