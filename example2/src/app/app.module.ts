841d9e6cf6dd1ba526fcfe17807cf62aabed91fe
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-05-12 11:18:37 +0300

    Routing

--- example2/src/app/app.module.ts
+++ example2/src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { UsersComponent } from './users/users.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { Route } from '@angular/compiler/src/core';
+import { NotfoundComponent } from './notfound/notfound.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent }, // localhost:4200
+  {path: 'home', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'users', component: UsersComponent },
+  {path: '**', component: NotfoundComponent}
+]
 
 @NgModule({
   declarations: [
@@ -13,10 +24,12 @@ import { CategoriesComponent } from './categories/categories.component';
     HomeComponent,
     ProductsComponent,
     UsersComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    NotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
